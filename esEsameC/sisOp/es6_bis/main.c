ffab0a82a44afa69333b5468c6c321a54e329265
gorgeous_george <vetrocemento@gmail.com>
2016-08-02 22:10:15 +0200

    Added/Modified comments on es6_bis/main.c

--- esEsameC/sisOp/es6_bis/main.c
+++ esEsameC/sisOp/es6_bis/main.c
@@ -9,9 +9,7 @@
 //numero di operatori nell'agenzia di viaggio
 #define OPERATORS 3 
 
-//semaforo che 
 pthread_mutex_t db_lock = PTHREAD_MUTEX_INITIALIZER;
-//semaforo che garantisce mutua esclusione all'accesso degli operatori al db
 pthread_mutex_t op_lock = PTHREAD_MUTEX_INITIALIZER;
 
 int seats=TRIP_SIZE;
@@ -39,7 +37,7 @@ void main(int argc, char *args){
 	printf("Agenzia chiusa!\n");
 }
 
-//inizializza trip[] e op[]
+//inizializza i threads operatore 
 void *inizializza(){
 	int j;
 
@@ -48,67 +46,87 @@ void *inizializza(){
 	}
 }
 
+//body dei threads operatore 
 void *operatore(){
 	while(1){
-		//generatore di numeri random usato da wait_client() e ask_confirmation()
+		//inizializza il generatore di numeri random 
+		//usato dai metodi wait_client() e ask_confirmation()
 		srand(time(NULL));
-		//attende un cliente
+
 		wait_client();
-		//proteggo l'accesso dell'operatore al db 
+
 		pthread_mutex_lock(&op_lock);
 		active_op++;
+		//se e' il primo operatore attivo sul database ne acquisisce il lock
+		//per consultarlo in cerca di posti liberi
 		if (active_op==1){
 			pthread_mutex_lock(&db_lock);
 		}
 		pthread_mutex_unlock(&op_lock);
-		//controllo disponibilita'
+
+		//controllo disponibilita' posti
 		if (check_availability()){
+			//Posto disponibile.
+			//terminata la consultazione del db, rilascia il lock sul db se
+			//e' l'ultimo operatore attivo
 			pthread_mutex_lock(&op_lock);
 			active_op--;
 			if (active_op==0){
 				pthread_mutex_unlock(&db_lock);
 			}
 			pthread_mutex_unlock(&op_lock);
-			//se il posto e' disponibile chiedo conferma della prenotazione
+
+			//chiedo conferma della prenotazione
 			if (ask_confirmation()){
+				//prenotazione confermata
+				//tenta di acquisire lock sul db per prenotare il posto
 				pthread_mutex_lock(&db_lock);
-				//avuta la conferma dal cliente, ricontrollo se il posto sia ancora libero
+				//l'operatore ha il lock: ricontrolla se il posto sia ancora libero
 				if (check_availability()){	
-					//ricevuta la conferma, prenoto il posto e rilascio il lock sul db
+					//prenota il posto e rilascia il lock sul db
 					book_seat();
 					pthread_mutex_unlock(&db_lock);
-					//confermo al cliente l'avvenuta prenotazione
+					//conferma al cliente l'avvenuta prenotazione
 					confirm_booking();
 				}
 				else {
-				pthread_mutex_unlock(&db_lock);
-				printf("Ci dispiace, qualcuno ha prenotato il posto prima di lei\n");
+					//qualcuno e' stato piu' rapido a confermare e sono finiti i posti.
+					//Rilascia il lock sul db
+					pthread_mutex_unlock(&db_lock);
+					printf("Ci dispiace, qualcuno ha prenotato il posto prima di lei\n");
 				}
 			}
 			else {
-				//prenotazione non confermata, rilascio il lock sul db
+				//prenotazione non confermata
+
 				pthread_mutex_lock(&op_lock);
 				active_op--;
+				//se era l'ultimo operatore attivo sul database, rilascia il lock sul db
 				if (active_op==0){
 					pthread_mutex_unlock(&db_lock);
 				}
 				pthread_mutex_unlock(&op_lock);
+
 				printf("Ci dispiace che abbia cambiato idea, speriamo di rivederla al piu' presto!\n");
 			}
 		}
 		else {	
+			//non ci sono posti disponibili.
 			pthread_mutex_lock(&op_lock);
 			active_op--;
+			//se era l'ultimo operatore attivo sul database, rilascia il lock sul db
 			if (active_op==0){
 				pthread_mutex_unlock(&db_lock);
 			}
 			pthread_mutex_unlock(&op_lock);
 			printf("Ci dispiace, il viaggio che le interessa e' gia' al completo! Speriamo di rivederla presto.\n");
+
 			break;
 		}
 	}
 }
 
+//simula l'attesa di un cliente
 void *wait_client(){
 	int i;
 	int delay;
@@ -118,23 +136,25 @@ void *wait_client(){
 	}
 }
 
+//ritorna 1 se ci sono posti disponibili, 0 altrimenti
 int check_availability(){
-	//ritorna 1 se ci sono posti disponibili, 0 altrimenti
 	int availability = seats>0 ? 1 : 0;
 	return availability;	
 }
 
+//genera un numero float random compreso tra [0;1]: 
+//se appartiene a [0;0.75) viene confermata la prenotazione.
 int ask_confirmation(){
-	//la decisione viene simulata generando un numero float random compreso tra [0;1]: 
-	//se appartiene a [0;0.75) viene confermata la prenotazione.
 	int confirmation = (float)((double)rand()/(double)RAND_MAX)<0.75 ? 1:0;	
 	return confirmation;
 }
 
+//prenota un posto 
 void *book_seat(){
 	seats--;
 }
 
+//conferma l'avvenuta prenotazione
 void *confirm_booking(){
 	printf("L'operatore %u effettua una prenotazione!\n",pthread_self());
 }
