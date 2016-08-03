0f77ac314a6620661d2aa6d6fe8fe0e9b2a3f7d8
gorgeous_george <vetrocemento@gmail.com>
2016-08-03 14:23:01 +0200

    Added comments to BlockingBuffer.java

--- esEsameJava/Incrocio/BlockingBuffer.java
+++ esEsameJava/Incrocio/BlockingBuffer.java
@@ -1,5 +1,6 @@
 import java.util.*;
 
+//Contiene una LinkedList e offre un metodo put() ed un metodo take(), entrambi thread-safe, per accedervi
 class BlockingBuffer{
 	LinkedList buf;
 	int MAX_SIZE;
@@ -8,11 +9,13 @@ class BlockingBuffer{
 		this.buf = new LinkedList();
 		this.MAX_SIZE = size;
 	}
-
+	
+	//put() prende in input una stringa token da aggiungere al buffer, ed una string msg da stampare a terminale
 	public synchronized void put( String token, String msg ) throws InterruptedException{
 		if( msg!="" ){
 			System.out.println( msg );
 		}
+		//attende che vi siano dei posti liberi nel buffer
 		while( buf.size()==MAX_SIZE ){
 			wait();
 		}
@@ -20,8 +23,10 @@ class BlockingBuffer{
 		notifyAll();
 	}
 
+	//take() prende in input una string msg da stampare a terminale, e ritorna la string prelevata dal buffer
 	public synchronized String take( String msg ) throws InterruptedException{
 		String token;
+		//attende che il buffer sia non vuoto
 		while( buf.size()==0 ){
 			wait();
 		}
